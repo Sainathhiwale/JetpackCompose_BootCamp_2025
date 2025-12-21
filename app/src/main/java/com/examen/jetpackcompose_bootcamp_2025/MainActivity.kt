47da5242b880e400f9aa7228ab5d200a479c5dcf
Sainathhiwale <sainathhiwale2@gmail.com>
2025-12-21 12:03:26 +0530

    Jetpack compose splash screen code is integrated.

--- app/src/main/java/com/examen/jetpackcompose_bootcamp_2025/MainActivity.kt
+++ app/src/main/java/com/examen/jetpackcompose_bootcamp_2025/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.examen.jetpackcompose_bootcamp_2025.ui.theme.JetpackCompose_BootCamp_2025Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             JetpackCompose_BootCamp_2025Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -38,10 +39,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackCompose_BootCamp_2025Theme {
         Greeting("Android")
     }
-}
+}*/
