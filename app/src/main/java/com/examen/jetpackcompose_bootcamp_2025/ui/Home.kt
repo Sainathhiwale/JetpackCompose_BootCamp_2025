821b7d1e9ede112a45ca2032a53aadf26dbd49d3
Sainathhiwale <sainathhiwale2@gmail.com>
2026-04-11 13:50:22 +0530

    navigation drawer with appbar is integrated

---
+++ app/src/main/java/com/examen/jetpackcompose_bootcamp_2025/ui/Home.kt
@@ -0,0 +1,29 @@
+package com.examen.jetpackcompose_bootcamp_2025.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.examen.jetpackcompose_bootcamp_2025.ui.theme.GreenSG
+
+@Composable
+fun Home() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+           Text("Home", fontSize = 30.sp,color = GreenSG)
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun homeNavigationDrawerPreview() {
+    Home()
+}
